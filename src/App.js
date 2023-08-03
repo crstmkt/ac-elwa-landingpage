f1c385ef03c744ab3a44122c4a940dbdb97b683e
Christian Mertenskötter <christian.mertenskoetter@ssi-plastic.com>
2023-08-03 12:48:44 +0200

    animate color

--- src/App.js
+++ src/App.js
@@ -17,16 +17,35 @@ function App() {
   }, []);
 
   const getBackgroundColor = (temperature) => {
-    if (temperature < 470) {
-      return "blue";
-    } else if (temperature < 570) {
-      return "#5553E8";
-    } else if (temperature < 670) {
-      return "#FF77A6";
+    const minTemperature = 350;
+    const maxTemperature = 830;
+
+    if (temperature <= minTemperature) {
+      return "#0300FF"; // Blau (#0300FF) für Temperaturen unter oder gleich 35 Grad
+    } else if (temperature >= maxTemperature) {
+      return "#FF0000"; // Rot (#FF0000) für Temperaturen über oder gleich 83 Grad
     } else {
-      return "red";
+      // Berechne den Farbverlauf zwischen Blau und Rot basierend auf der Temperatur
+      const percentage =
+        (temperature - minTemperature) / (maxTemperature - minTemperature);
+      const red = Math.round(percentage * 255);
+      const blue = Math.round((1 - percentage) * 255);
+
+      return `rgb(${red}, 0, ${blue})`;
     }
   };
+
+  // const getBackgroundColor = (temperature) => {
+  //   if (temperature < 470) {
+  //     return "blue";
+  //   } else if (temperature < 570) {
+  //     return "#5553E8";
+  //   } else if (temperature < 670) {
+  //     return "#FF77A6";
+  //   } else {
+  //     return "red";
+  //   }
+  // };
 
   return (
     <div className="container">
